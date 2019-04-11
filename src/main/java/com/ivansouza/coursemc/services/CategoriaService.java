323fc8f6ba4852aa2a65385de88c0b94ec247bb9
Ivan Souza <icsouza@yahoo.com>
2019-04-11 09:50:49 -0300

    Paginação com parâmetros opcionais na requisição

--- src/main/java/com/ivansouza/coursemc/services/CategoriaService.java
+++ src/main/java/com/ivansouza/coursemc/services/CategoriaService.java
@@ -5,10 +5,12 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.ivansouza.coursemc.domain.Categoria;
-import com.ivansouza.coursemc.dto.CategoriaDTO;
 import com.ivansouza.coursemc.repositories.CategoriaRepository;
 import com.ivansouza.coursemc.services.exceptions.DataIntegrityException;
 import com.ivansouza.coursemc.services.exceptions.ObjectNotFoundException;
@@ -50,6 +52,12 @@ public class CategoriaService {
 		catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
 		}
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+		
 	}
 
 }
