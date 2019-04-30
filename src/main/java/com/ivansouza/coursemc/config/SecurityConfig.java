4ed0ff09b7288d2a0bdf4332cfb57417cf437c77
Ivan Souza <icsouza@yahoo.com>
2019-04-30 15:19:17 -0300

    Implementando autorizacao

--- src/main/java/com/ivansouza/coursemc/config/SecurityConfig.java
+++ src/main/java/com/ivansouza/coursemc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.ivansouza.coursemc.security.JWTAuthenticationFilter;
+import com.ivansouza.coursemc.security.JWTAuthorizationFilter;
 import com.ivansouza.coursemc.security.JWTUtil;
 
 @Configuration
@@ -59,6 +60,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
