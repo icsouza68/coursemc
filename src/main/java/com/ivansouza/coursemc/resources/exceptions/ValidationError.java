94b5ad39a786056983da7f59239fe9a45f810c91
Ivan Souza <icsouza@yahoo.com>
2019-05-03 17:49:20 -0300

    Padronizando formato das excecoes

--- src/main/java/com/ivansouza/coursemc/resources/exceptions/ValidationError.java
+++ src/main/java/com/ivansouza/coursemc/resources/exceptions/ValidationError.java
@@ -4,15 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ValidationError extends StandardError {
-
-	private List<FieldMessage> errors = new ArrayList<FieldMessage>();
+	private static final long serialVersionUID = 1L;
 	
-	public ValidationError(Integer status, String msg, Long timestamp) {
-		super(status, msg, timestamp);
-		// TODO Auto-generated constructor stub
+	private List<FieldMessage> errors = new ArrayList<>();
+	
+	
+	public ValidationError(Long timestamp, Integer status, String error, String message, String path) {
+		super(timestamp, status, error, message, path);
 	}
 
-	public List<FieldMessage> getError() {
+	public List<FieldMessage> getErrors() {
 		return errors;
 	}
 
