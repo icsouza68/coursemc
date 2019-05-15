0461e14ccba30ccc59d2e7dce9b98840fe16f535
Ivan Souza <icsouza@yahoo.com>
2019-05-15 11:04:10 -0300

    Documentacao com Swagger - Parte 1

--- src/main/java/com/ivansouza/coursemc/config/SecurityConfig.java
+++ src/main/java/com/ivansouza/coursemc/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -27,72 +28,83 @@ import com.ivansouza.coursemc.security.JWTUtil;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
 	@Autowired
 	private UserDetailsService userDetailsService;
-	
+
 	@Autowired
 	private Environment env;
-	
+
 	@Autowired
 	private JWTUtil jwtUtil;
-	
-	private static final String[] PUBLIC_MATCHERS = {
-		"/h2-console/**",
+
+	private static final String[] PUBLIC_MATCHERS = { 
+			"/h2-console/**" 
 	};
-	
+
 	// caminhos apenas de leitura
-	private static final String[] PUBLIC_MATCHERS_GET = {
-			"/produtos/**",
-			"/categorias/**",
-			"/estados/**"
+	private static final String[] PUBLIC_MATCHERS_GET = { 
+			"/produtos/**", 
+			"/categorias/**", 
+			"/estados/**" 
 	};
 
 	// caminhos apenas de update
-	private static final String[] PUBLIC_MATCHERS_POST = {
-			"/clientes",
-			"/auth/forgot/**"
+	private static final String[] PUBLIC_MATCHERS_POST = { 
+			"/clientes", 
+			"/auth/forgot/**" 
 	};
+
+	// Essa configuração é pra permitir que a aplicação acesse o swagger, pois usamos o Spring Security
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers(
+				"/v2/api-docs", 
+				"/configuration/ui", 
+				"/swagger-resources/**", 
+				"/configuration/**",
+				"/swagger-ui.html", 
+				"/webjars/**");
+	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		
+
 		// Comando específico para usar o banco de dados H2
 		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
 			http.headers().frameOptions().disable();
 		}
-		
+
 		http.cors().and().csrf().disable();
-		http.authorizeRequests()
-			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
-			.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
-			.antMatchers(PUBLIC_MATCHERS).permitAll()
-			.anyRequest().authenticated();
+		http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+				.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll().antMatchers(PUBLIC_MATCHERS).permitAll()
+				.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
 		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
-	
+
 	@Override
-	public void configure (AuthenticationManagerBuilder auth) throws Exception {
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
 	}
-	
+
 	// o bean abaixo faz parte da solução que o instrutor pegou na internet
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
-/*
- "Adds a CorsFilter to be used. If a bean by the name of corsFilter is provided, that CorsFilter is
-used. Else if corsConfigurationSource is defined, then that CorsConfiguration is used. Otherwise, if
-Spring MVC is on the classpath a HandlerMappingIntrospector is used."
- */
+		/*
+		 * "Adds a CorsFilter to be used. If a bean by the name of corsFilter is
+		 * provided, that CorsFilter is used. Else if corsConfigurationSource is
+		 * defined, then that CorsConfiguration is used. Otherwise, if Spring MVC is on
+		 * the classpath a HandlerMappingIntrospector is used."
+		 */
 		CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
 		configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
 		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", configuration);
 		return source;
 	}
-	
+
 	@Bean
 	BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
